d38e6167845ab82fadf7ab994a2cf310bf9fb93f
qaoechi <junseong10683@gmail.com>
2025-12-25 00:48:03 +0900

    init test

---
+++ src/main/java/com/jumjari/zobiac/ZobiacApplication.java
@@ -0,0 +1,13 @@
+package com.jumjari.zobiac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZobiacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZobiacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jumjari/zobiac/ZobiacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumjari.zobiac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZobiacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
