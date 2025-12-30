6c0d081ada6842c9868250930f58d374157f826d
qaoechi <junseong10683@gmail.com>
2025-12-31 00:46:19 +0900

    fix: re-enable uncomment code in naver map

--- src/main/java/com/jumjari/zobiac/application/building/service/BuildingSearchService.java
+++ src/main/java/com/jumjari/zobiac/application/building/service/BuildingSearchService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
