af95a883ee36f8a449e1de0d0914935aba436797
qaoechi <junseong10683@gmail.com>
2026-03-03 17:18:18 +0900

    feat: add week data to cell dataset

--- src/main/resources/static/js/schedule-slot.js
+++ src/main/resources/static/js/schedule-slot.js
@@ -1,4 +1,5 @@
 const tbody = document.querySelector("#time-table tbody");
+const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 for (let hour = 9; hour < 22; hour++) {
     for (let min of [0, 30]) {
@@ -8,13 +9,13 @@ for (let hour = 9; hour < 22; hour++) {
         timecell.innerText = `${hour.toString().padStart(2, '0')}:${min === 0 ? "00" : "30"}`;
         tr.appendChild(timecell);
 
-        for (let week = 0; week < 7; week++) {
-
+        week.forEach(element => {
             const td = document.createElement("td");
+            td.dataset.week = element;
             td.dataset.time = timecell.innerText;
             td.classList.add("time-slot");
             tr.appendChild(td);
-        }
+        });
 
         tbody.appendChild(tr);
     }
