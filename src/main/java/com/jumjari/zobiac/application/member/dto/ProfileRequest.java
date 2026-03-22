f7e557afc30d1ef213ea985623fd68cade9685de
qaoechi <junseong10683@gmail.com>
2026-03-23 01:42:02 +0900

    refactor: Adjust profile form code to match refactored structure

--- src/main/java/com/jumjari/zobiac/application/member/dto/ProfileRequest.java
+++ src/main/java/com/jumjari/zobiac/application/member/dto/ProfileRequest.java
@@ -1,7 +1,14 @@
 package com.jumjari.zobiac.application.member.dto;
 
-public record ProfileRequest(
-    String username,
-    Integer number,
-    String nickname
-) {}
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProfileRequest{
+    private String username;
+    private Integer number;
+    private String nickname;
+}
