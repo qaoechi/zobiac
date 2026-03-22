e0769af2a0b82baf34940cb22cb1a16aec948dcb
qaoechi <junseong10683@gmail.com>
2026-03-22 23:35:39 +0900

    refactor: Add member route

--- src/main/java/com/jumjari/zobiac/api/client/member/controller/MemberController.java
+++ src/main/java/com/jumjari/zobiac/api/client/member/controller/MemberController.java
@@ -16,14 +16,13 @@ public class MemberController {
 
     @GetMapping("/login")
     public String loginPage() {
-        return "member";
+        return "layout/member";
     }
 
     @PostMapping("/logout")
     public String logout(
         HttpServletResponse response
     ) {
-
         return "redirect:/home";
     }
 }
